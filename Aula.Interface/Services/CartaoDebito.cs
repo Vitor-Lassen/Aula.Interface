af9941b7d82de7c8dbf36ebe31bca509c6beacf2
Vitor Lassen <vitor@lassen.com.br>
2022-06-26 13:27:31 -0300

    Base files for demo

---
+++ Aula.Interface/Services/CartaoDebito.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula.Interface.Services
+{
+    public class CartaoDebito
+    {
+        private double _saldo;
+        public double ConsultarSaldo()
+        {
+            return _saldo;
+        }
+
+        public void Creditar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public void Debitar(double valor)
+        {
+            _saldo -= valor;
+        }
+    }
+}
